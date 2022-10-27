386434bc97873efefe8e0fe865f174aad3fd326b
Raxabi <beltrandiaztorres@gmail.com>
2022-10-27 22:58:51 +0000

    [EDIT] Edited function name 'AddNewTask' by 'addNewTask'

--- src/components/utils/addNewTask.js
+++ src/components/utils/addNewTask.js
@@ -1,4 +1,4 @@
 // Define function that add a new task
-export function AddNewTask(tasks, contentOfNewTask) {
+export function addNewTask(tasks, contentOfNewTask) {
     setNewInitialTasks(tasks.concat([{content: contentOfNewTask}]))
 }
